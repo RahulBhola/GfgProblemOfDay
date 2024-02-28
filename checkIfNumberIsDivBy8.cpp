2616dfe97b384c9767ce96368ce07128b7d6df2e
Rahul Bhola <104344946+RahulBhola@users.noreply.github.com>
2024-02-28 18:58:20 +0530

    checkIfNumberIsDivBy8.cpp

---
+++ checkIfNumberIsDivBy8.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    int DivisibleByEight(string s){
+        //code here
+        if(s.size()<=3){
+            return stoi(s)%8==0?1:-1;
+        }
+        return stoi(s.substr(s.size()-3))%8==0?1:-1;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string S;
+        cin>>S;
+        Solution ob;
+        cout<<ob.DivisibleByEight(S)<<"\n";
+    }
+}
+// } Driver Code Ends
